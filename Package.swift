6471b4fd1ca73907b70ea73d05ce62f3c33715e3
John Pusey <ebardx@gmail.com>
2026-06-26 09:26:22 -0400

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.3
 
 // © 2022–2026 John Gary Pusey (see LICENSE.md)
 
@@ -18,7 +18,7 @@ let package = Package(name: "XestiXML",
                       products: [.library(name: "XestiXML",
                                           targets: ["XestiXML"])],
                       dependencies: [.package(url: "https://github.com/eBardX/XestiTools.git",
-                                              .upToNextMajor(from: "7.3.0"))],
+                                              .upToNextMajor(from: "9.0.0"))],
                       targets: [.target(name: "XestiXML",
                                         dependencies: [.product(name: "XestiTools",
                                                                 package: "XestiTools")],
